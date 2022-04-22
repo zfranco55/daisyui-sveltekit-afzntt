9eb850d326a10f45861c02db34b804dc92d50f09
zfranco55 <znfranco@gmail.com>
2022-04-22 15:44:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-sveltekit-afzntt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-sveltekit-afzntt)
---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<div class="dropdown">
+  <label tabindex="0" class="btn m-1">Click</label>
+  <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a href="www.google.it">Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+  </div>
+  <br>
+  <br>
+  <div>
+  <input type="date" placeholder="data" class="input w-full max-w-xs">
+</div>
